96cf4e90285bf97cc033960adcd7a441b243e79e
Daiana Turetska <turetska.daiana@chnu.edu.ua>
2022-03-10 23:29:15 +0200

    modified

--- HeapSort/HeapSort.java
+++ HeapSort/HeapSort.java
@@ -2,48 +2,34 @@ public class HeapSort {
 	public void sort(int arr[])
 	{
 		int n = arr.length;
-
-		// Build heap 
 		for (int i = n / 2 - 1; i >= 0; i--)
-			heapify(arr, n, i);
+			heap(arr, n, i);
 
-		// One by one extract an element from heap
 		for (int i = n - 1; i > 0; i--) {
-			// Move current root to end
-			int temp = arr[0];
+			int buff = arr[0];
 			arr[0] = arr[i];
-			arr[i] = temp;
-
-			// call max heapify on the reduced heap
-			heapify(arr, i, 0);
+			arr[i] = buff;
+			heap(arr, i, 0);
 		}
 	}
 
-	void heapify(int arr[], int n, int i)
+	void heap(int arr[], int n, int i)
 	{
-		int largest = i; // Initialize largest as root
-		int l = 2 * i + 1; // left = 2*i + 1
-		int r = 2 * i + 2; // right = 2*i + 2
+		int biggest = i; 
+		int left = 2 * i + 1; 
+		int right = 2 * i + 2; 
 
-		// If left child is larger than root
-		if (l < n && arr[l] > arr[largest])
-			largest = l;
-
-		// If right child is larger than largest so far
-		if (r < n && arr[r] > arr[largest])
-			largest = r;
-
-		// If largest is not root
-		if (largest != i) {
+		if (left < n && arr[left] > arr[biggest])
+			biggest = left;
+		if (right < n && arr[right] > arr[biggest])
+			biggest = right;
+		if (biggest != i) {
 			int swap = arr[i];
-			arr[i] = arr[largest];
-			arr[largest] = swap;
-
-			// Recursively heapify the affected sub-tree
-			heapify(arr, n, largest);
+			arr[i] = arr[biggest];
+			arr[biggest] = swap;
+			heap(arr, n, biggest);
 		}
 	}
-
 	static void printArray(int arr[])
 	{
 		int n = arr.length;
@@ -52,16 +38,17 @@ public class HeapSort {
 		System.out.println();
 	}
 
-	// Driver code
 	public static void main(String args[])
 	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
+		int arr[] = { 22, 11, 18, 8, 9, 13 };
+		System.out.println("Array:");
+		printArray(arr);
 		int n = arr.length;
 
 		HeapSort ob = new HeapSort();
 		ob.sort(arr);
 
-		System.out.println("Sorted array is");
+		System.out.println("Sorted array :");
 		printArray(arr);
 	}
 }
